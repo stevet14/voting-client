4da5ee77bab63da699551a40994ccb0c1638d0df
stevet14 <steve.taplin@gmail.com>
2017-05-17 18:46:52 +0100

    Fixed most tests

--- test/components/Voting_spec.jsx
+++ test/components/Voting_spec.jsx
@@ -48,6 +48,28 @@ describe('Voting', () => {
     expect(buttons[0].textContent).to.contain('Voted');
   });
 
+  it('disables buttons when user has voted', () => {
+    const component = renderIntoDocument(
+      <Voting pair={["Trainspotting", "28 Days Later"]}
+              hasVoted="Trainspotting" />
+    );
+    const buttons = scryRenderedDOMComponentsWithTag(component, 'button');
+
+    expect(buttons.length).to.equal(2);
+    expect(buttons[0].hasAttribute('disabled')).to.equal(true);
+    expect(buttons[1].hasAttribute('disabled')).to.equal(true);
+  });
+
+  it('adds label to the voted entry', () => {
+    const component = renderIntoDocument(
+      <Voting pair={["Trainspotting", "28 Days Later"]}
+              hasVoted="Trainspotting" />
+    );
+    const buttons = scryRenderedDOMComponentsWithTag(component, 'button');
+
+    expect(buttons[0].textContent).to.contain('Voted');
+  });
+
   it('renders just the winner when there is one', () => {
     const component = renderIntoDocument(
       <Voting winner="Trainspotting" />
